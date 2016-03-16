711d90cb073e362c622a85f9b9e93535f87aabf5
daniel-williams <mail@danielwilliams.us>
2016-03-15 20:38:43 -0700

    Rewrite service to use ngrx store and observables.

--- src/services/blog.service.ts
+++ src/services/blog.service.ts
@@ -1,22 +1,36 @@
-import {Http} from 'angular2/http';
 import {Injectable} from 'angular2/core';
+import {Http} from 'angular2/http';
 import {Observable} from 'rxjs';
 import 'rxjs/add/operator/map';
+import {Store} from '@ngrx/store';
 
+import * as actions from '../actions/blog.action';
 import {appSettings} from '../app.settings';
-import {BlogArticle} from '../interfaces';
+import {AppState, BlogState, BlogArticle} from '../interfaces';
 
 
 @Injectable()
 export class BlogService {
+  blog: Observable<BlogState>;
   
-  constructor(private _http: Http) {
+  constructor(private _http: Http, private store: Store<AppState>) {
+    this.blog = store.select(state => state.blog);
   }
   
-  public getExcepts(): Observable<BlogArticle[]> {
+  public loadExcepts() {
     return this._http.get(appSettings.blog.EXCERPTS)
       .map(res => res.json())
-      .map(res => res.map(item => this.parseExcerpt(item)));
+      .map(res => res.map(item => this.parseExcerpt(item)))
+      .map(posts => {
+        return {
+          type: actions.FETCH_EXCERPTS_SUCCESS,
+          payload: {
+            date: new Date(),
+            posts: posts
+          }
+        }
+      })
+      .subscribe(action => this.store.dispatch(action));
   }
   
   private parseExcerpt(item: any): BlogArticle {
