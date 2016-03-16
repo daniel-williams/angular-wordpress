e044accc20163e43d630cb7dfc0a03e98504d718
daniel-williams <mail@danielwilliams.us>
2016-03-15 20:35:02 -0700

    Switch to ngrx for redux-like state management. Updated css autoprefixer per offical recommendations.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
 
 var PROJECT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(PROJECT_PATH, 'src');
@@ -8,41 +9,44 @@ var DIST_PATH = path.resolve(WEB_PATH, 'content/bundles');
 
 
 module.exports = {
-    entry: {
-        'app': [
-          'webpack-hot-middleware/client',
-          path.join(APP_PATH, 'main.ts')
-        ],
-        'vendors': path.join(APP_PATH, 'vendors.ts')
-    },
-    output: {
-        path: DIST_PATH,
-        filename: '[name].bundle.js',
-        publicPath: '/content/bundles/'
-    },
-    module: {
-        loaders: [{
-            test: /\.ts$/,
-            loader: 'ts',
-            exclude: [
-                /node_modules/
-            ]
-        }, {
-            test: /\.json$/,
-            loader: "json"
-        }, {
-            test: /\.html$/,
-            loader: 'raw'
-        }, {
-            test: /\.scss$/,
-            loader: "raw!autoprefixer?browsers=last 2 versions!sass"
-        }],
-        noParse: [/angular2\/bundles\/.+/],
-    },
-    resolve: {
-        extensions: ['', '.ts', '.js', '.html', '.scss']
-    },
-    plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-    ]
+  devtool: 'source-map',
+  debug: true,
+  entry: {
+    'app': [
+      'webpack-hot-middleware/client',
+      path.join(APP_PATH, 'main.ts')
+    ],
+    'vendors': path.join(APP_PATH, 'vendors.ts')
+  },
+  output: {
+    path: DIST_PATH,
+    filename: '[name].bundle.js',
+    publicPath: '/content/bundles/'
+  },
+  module: {
+    loaders: [{
+      test: /\.ts$/,
+      loader: 'ts',
+      exclude: [
+          /node_modules/
+      ]
+    }, {
+      test: /\.json$/,
+      loader: "json"
+    }, {
+      test: /\.html$/,
+      loader: 'raw'
+    }, {
+      test: /\.scss$/,
+      loader: "raw!postcss!sass"
+    }],
+    postcss: function() { return [autoprefixer({browsers:['last 2 versions']})]; },
+    noParse: [/angular2\/bundles\/.+/],
+  },
+  resolve: {
+    extensions: ['', '.ts', '.js', '.html', '.scss']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ]
 };
