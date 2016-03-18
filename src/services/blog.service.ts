cfb7fca7fd7ab878d4739945bc4e6f44124aa711
daniel-williams <mail@danielwilliams.us>
2016-03-18 13:50:19 -0700

    Rewrite blog service using @ngrx and observables patterns.

--- src/services/blog.service.ts
+++ src/services/blog.service.ts
@@ -1,42 +1,60 @@
 import {Injectable} from 'angular2/core';
-import {Http} from 'angular2/http';
+import {Headers, Http, Request, RequestMethod, Response} from 'angular2/http';
 import {Observable} from 'rxjs';
-import 'rxjs/add/operator/map';
-import {Store} from '@ngrx/store';
 
-import * as actions from '../actions/blog.action';
 import {appSettings} from '../app.settings';
-import {AppState, BlogState, BlogArticle} from '../interfaces';
+import * as actions from '../actions/blog.action';
+import {
+  IAppState,
+  IBlogState,
+  IBlogArticle
+} from '../interfaces';
 
 
 @Injectable()
 export class BlogService {
-  blog: Observable<BlogState>;
   
-  constructor(private _http: Http, private store: Store<AppState>) {
-    this.blog = store.select(state => state.blog);
+  constructor(private http: Http) {
   }
   
-  public loadExcepts() {
-    return this._http.get(appSettings.blog.EXCERPTS)
-      .map(res => res.json())
-      .map(res => res.map(item => this.parseExcerpt(item)))
-      .map(posts => {
-        return {
-          type: actions.FETCH_EXCERPTS_SUCCESS,
-          payload: {
-            date: new Date(),
-            posts: posts
-          }
-        }
-      })
-      .subscribe(action => this.store.dispatch(action));
+  public loadExcepts(): Observable<IBlogState> {
+    return this.request({
+      method: RequestMethod.Get,
+      url: appSettings.blog.EXCERPTS
+    }).map(res => res.map(item => this.parseExcerpt(item)));
+    // return this.http.get(appSettings.blog.EXCERPTS)
+    //   .map(res => res.json())
+    //   .map(res => res.map(item => this.parseExcerpt(item)))
+    //   .map(posts => {
+    //     return {
+    //       type: actions.FETCH_EXCERPTS_SUCCESS,
+    //       payload: {
+    //         date: new Date(),
+    //         posts: posts
+    //       }
+    //     }
+    //   });
   }
   
-  private parseExcerpt(item: any): BlogArticle {
-    return <BlogArticle> Object.assign({}, item, {isLoaded: false, date: new Date(item.date)});
+  private parseExcerpt(item: any): IBlogArticle {
+    return <IBlogArticle> Object.assign({}, item, {isLoaded: false, date: new Date(item.date)});
   }
+  
+  request(options: any): Observable<any> {
+    if (options.body) {
+      if (typeof options.body !== 'string') {
+        options.body = JSON.stringify(options.body);
+      }
 
+      options.headers = new Headers({
+        'Content-Type': 'application/json'
+      });
+    }
+
+    return this.http
+      .request(new Request(options))
+      .map((res: Response) => res.json());
+  }
 }
 
 
