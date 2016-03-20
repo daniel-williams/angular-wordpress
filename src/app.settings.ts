e34ccf1ef957b908f9e26629a886c17f21fa027f
daniel-williams <mail@danielwilliams.us>
2016-03-19 19:01:07 -0700

    Change blog post endpoint to take id as param.

--- src/app.settings.ts
+++ src/app.settings.ts
@@ -12,7 +12,9 @@ class AppSettings {
 class BlogEndpoints {
   public API_ROOT: string = 'http://blogabit.com/wp-json/wp/v2/';
   public EXCERPTS: string = this.API_ROOT + 'posts?_query=[].{id:id,date:date,title:title.rendered,slug:slug,excerpt:excerpt.rendered}';
-  public POSTS: string = this.API_ROOT + 'posts/';
+  public getPostBodyEndpoint(id: number): string {
+    return `${this.API_ROOT}posts/${id}?_query={id:id,content:content.rendered}`;
+  }
 }
 
 export const appSettings: AppSettings = new AppSettings();
