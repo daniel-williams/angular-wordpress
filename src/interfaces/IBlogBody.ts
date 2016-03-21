1d58054be10554e0cd2ae87bcfead22048d3649f
daniel-williams <mail@danielwilliams.us>
2016-03-21 13:35:39 -0700

    Consolidate and refactoring blog interfaces.

--- src/interfaces/IBlogBody.ts
+++
@@ -1,4 +0,0 @@
-export interface IBlogBody {
-  id: number,
-  content: string,
-}
--- src/interfaces/IBlogStore.ts
+++ src/interfaces/IBlogStore.ts
@@ -1,12 +1,48 @@
-import {IBlogPost} from './IBlogPost';
 
-export interface IBlogStore{
+export interface IBlogStore {
     isUpdating: boolean,
     date?: Date,
     error?: any,
+    
+    needTitles: boolean,
+    postMap?: any,
+    
     totalPostCount: number,
     itemsPerPage: number,
     totalPages: number,
     activePage: number,
-    posts: Array<IBlogPost>,
+}
+
+
+export interface IBlogPost {
+  id: number,
+  title: string,
+  slug: string,
+  date: Date,
+  
+  needSummary: boolean,
+  summary?: string,
+  
+  needBody: boolean,
+  body?: string,
+}
+
+
+export interface IBlogTitle {
+  id: number,
+  title: string,
+  slug: string,
+  date: Date,
+}
+
+export interface IBlogSummary {
+  id: number,
+  summary: string,
+  needSummary: boolean,
+}
+
+export interface IBlogBody {
+  id: number,
+  body: string,
+  needBody: boolean,
 }
